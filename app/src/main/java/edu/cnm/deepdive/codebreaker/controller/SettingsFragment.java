7c1540ab526a9e48e4fe67f1fb3dacb1637f95b1
Adrian Anaya <aanaya79@cnm.edu>
2020-11-11 15:28:48 -0700

    Changed navigation

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SettingsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SettingsFragment.java
@@ -1,11 +1,6 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
 import android.os.Bundle;
-import android.view.View;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 import edu.cnm.deepdive.codebreaker.R;
 
@@ -14,15 +9,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.preferences, rootKey);
-  }
-
-  @SuppressWarnings("ConstantConditions")
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
-    actionBar.setDisplayHomeAsUpEnabled(true);
-    actionBar.setDisplayShowHomeEnabled(true);
   }
 
 }
